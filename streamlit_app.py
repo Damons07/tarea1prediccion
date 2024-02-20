efd3883af350a1e2bf2b6f3022ff078a3649dd82
Damons07 <damianedg666@gmail.com>
2024-02-19 19:59:26 -0600

    ya anda bien el nene (mapa)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,29 @@
 import streamlit as st
 import pandas as pd
 from pymongo import MongoClient
-import matplotlib.pyplot as plt
 
-# Conexion MongoDB
+#Conexion MongoDB
 client = MongoClient('mongodb+srv://damons07:mNcsCwuuA8vYuxB9@prediccion2024.htxnzwm.mongodb.net/')
 db = client.sample_geospatial
 collection = db.shipwrecks
 
-#Obtener datos de la colección
+#Obtener datos dl colección
 df = pd.DataFrame(list(collection.find()))
 
-#Mostrar opciones para el combo
+#opciones
 opciones = ["Sin limpieza", "Con limpieza"]
 opcion_seleccionada = st.sidebar.selectbox("Selecciona una opción:", opciones)
 
-# Mostrardataframe
+#sucio
 if opcion_seleccionada == "Sin limpieza":
     st.write("Dataframe sin limpieza:")
     st.write(df)
 else:
-    #Seleccionar las colunas
+    #aqui se limpia y se muestra
     df_limpio = df[["recrd", "latdec", "londec", "depth", "watlev", "feature_type"]]
-    df_limpio.columns = ["Registro", "Latitud", "Longitud", "Profundidad", "Nivel del agua", "Tipo de característica"]
+    df_limpio.columns = ["Registro", "LAT", "LON", "Profundidad", "Nivel del agua", "Tipo de característica"]
     st.write("Dataframe con limpieza:")
     st.write(df_limpio)
 
-    #grafico de pastel
-    tipocarac = df_limpio['Tipo de característica'].value_counts()
-    fig, ax = plt.subplots()
-    ax.pie(tipocarac, labels=tipocarac.index, autopct='%1.1f%%', startangle=140)
-    ax.set_title('Porcentaje de Tipo de Característica')
-    st.pyplot(fig)
+    #Mapa
+    st.map(df_limpio, color="#00BFBF")
